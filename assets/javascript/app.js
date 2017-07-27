81bc4280ce9147be8c01dc3d5efa9fd1f702434e
Dustin Bahr <dubahr@extendhealth.com>
2017-07-27 17:10:36 -0600

    added all the HTML and generated buttions with JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,33 @@
+var peopleArr = ["Homer Simpson", "Arnold Schwarzenegger", "Stephen Colbert", "Ryan Gosling"];
+var createPeopleButtons = function(){
+	for (var i = 0; i < peopleArr.length; i++) {
+		var person = peopleArr[i];
+		var newButton = $('<button class="btnPerson">');
+		newButton.attr("data-name", person.split(' ').join('+'));
+		newButton.text(person);
+		if(i==0){
+
+			$('#buttonHolder').html(newButton);
+
+		} else {
+
+			$('#buttonHolder').append(newButton);
+
+		};
+		
+	};
+}
+
+
+
+
 $('document').ready(function () {
 
-	$("body").on('click', '.btnAnswer', function () {
+	createPeopleButtons();
 
-		var answer = $(this).attr('data-correct');
+	$("body").on('click', '.btnPerson', function () {
+
+		var answer = $(this).attr('data-name');
 		
     });
     
