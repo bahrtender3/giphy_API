e3ed796c4bfd126a1e29ad78f7406c941d609cf9
Dustin Bahr <dbahr3@Dustins-MacBook-Pro.local>
2017-07-29 23:44:40 -0600

    made api a https so that it will work.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,7 +28,7 @@ $('document').ready(function () {
 	$("body").on('click', '.btnPerson', function () {
 
 		var person = $(this).attr("data-name");
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
 			person + "&api_key=dc6zaTOxFJmzC&limit=6";
 
 		$.ajax({
